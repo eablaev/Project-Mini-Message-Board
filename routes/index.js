593d774c413a4bc16e09380baf0e931fdd204792
shmerv <eaudioworks@gmail.com>
2024-04-02 10:07:05 -0700

    Finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const options = {month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date().toLocaleString([], options)
+    },
+    {
+      text: "Hey, whats up!",
+      user: "Charles",
+      added: new Date().toLocaleString([], options)
+    }
+ ];
+ 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chatroom', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+    res.render('form')
+});
+
+router.post('/new', function(req, res, next) {
+    messages.push({text: req.body.message, user: req.body.author, added: new Date().toLocaleString([], options)})
+    res.redirect('/') 
 });
 
 module.exports = router;
